8ee9e82b4035cba74a6f31557a52695da7e00ef6
Sara Bolduc <sara.s.bolduc@gmail.com>
2020-05-13 10:45:28 -0700

    adds js simple math demo files

---
+++ Week-3/intro-javascript/app.js
@@ -0,0 +1,20 @@
+var num1 = 10;
+var num2 = 2;
+console.log("num1 = ", num1);
+console.log("num2 = " + num2);
+
+// addition
+var add = num1 + num2;
+console.log("addition: " + num1 + " + " + num2 + " = " + add);
+
+// subtraction
+var sub = num1 - num2;
+console.log("subtraction:" + num1 + " - " + num2 + " = " + sub);
+
+// multiplication
+var mul = num1 * num2;
+console.log("multiplication: " + num1 + " * " + num2 + " = " + mul);
+
+// division
+var div = num1 / num2;
+console.log("division: " + num1 + " / " + num2 + " = " + div);
