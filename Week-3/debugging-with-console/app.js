637d1430322d48dcafadf3cc209e3112def317e2
Sara Bolduc <sara.s.bolduc@gmail.com>
2020-05-13 11:31:46 -0700

    adds debugging with console demo files

---
+++ Week-3/debugging-with-console/app.js
@@ -0,0 +1,17 @@
+// print a string
+console.log("hello World");
+
+// print a variable
+var name = "henry";
+console.log(name);
+
+// do math
+console.log(3 + 4);
+
+// do math with variables
+var num1 = 1;
+var num2 = 2;
+console.log(num1 + num2);
+
+// print strings & variables to the screen using concatenation (+)
+console.log(name + " is " + num2 + " years old.");
